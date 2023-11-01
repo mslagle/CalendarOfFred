5ce56d24754f515339db06e3c4b8fa2632c5f321
Matt Slagle <mslagle777@gmail.com>
2023-10-31 21:40:11 -0400

    Feature:  Create intial site

---
+++ CalendarOfFred/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CalendarOfFred.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
